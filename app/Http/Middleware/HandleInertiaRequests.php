208796429ebafee2f00a5a64c487d202070ea47f
Clebson Moura <clebson.sh@gmail.com>
2023-03-04 11:30:42 -0300

    Mostra o link do dashboard somente para admins

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,6 +33,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $request->user(),
+                'isAdmin' => $request->user()?->isA('superadmin'),
             ],
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
